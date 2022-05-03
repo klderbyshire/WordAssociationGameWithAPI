efd096083aa56f5a0e30937f20814506ab3b0110
karen derbyshire <karenderbyshire5@gmail.com>
2022-05-03 13:20:14 +0100

    code tidied up

---
+++ index.js
@@ -0,0 +1,38 @@
+const PORT = 3000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const axios = require("axios");
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("hi");
+});
+
+//get results of api to the backend
+app.get("/results", (req, res) => {
+  const passedLevel = req.query.level;
+
+  const options = {
+    method: "GET",
+    url: "https://twinword-word-association-quiz.p.rapidapi.com/type1/",
+    params: { level: passedLevel, area: "sat" },
+    headers: {
+      "x-rapidapi-host": "twinword-word-association-quiz.p.rapidapi.com",
+      "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
